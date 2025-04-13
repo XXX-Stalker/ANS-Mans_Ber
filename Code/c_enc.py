7337c2a1491dca1b4cc1d36eaef14bc9cede26aa
XXX-Stalker <2931727685@qq.com>
2025-04-13 09:50:30 +0800

    Update c_enc.py

--- Code/c_enc.py
+++ Code/c_enc.py
@@ -1,4 +1,4 @@
-def c_enc_main():
+def c_enc():
     while True:
         choice = input(":")
 
@@ -7,7 +7,7 @@ def c_enc_main():
             file_path = input("程序文件生成路径(文件夹路径!): ")
             file_name = input("程序文件名称(不需要后缀!): ")
             file_path_2 = f"{file_path}\{file_name}.py"
-            with open(r"c_enc_RSA\c_enc_folder_1.py", "r", encoding="utf-8") as f:
+            with open(r"plugins\c_enc_RSA\c_enc_folder_1.py", "r", encoding="utf-8") as f:
                 template = f.read()
             with open(file_path_2, "w", encoding="utf-8") as f:
                 f.write(f'folder_to_encrypt = r"{folder_to_encrypt}"\n')
@@ -18,7 +18,7 @@ def c_enc_main():
             file_path = input("程序文件生成路径(文件夹路径!): ")
             file_name = input("程序文件名称(不需要后缀!): ")
             file_path_2 = f"{file_path}/{file_name}.py"
-            with open(r"c_enc_RSA\c_enc_file_1.py", "r", encoding="utf-8") as f:
+            with open(r"plugins\c_enc_RSA\c_enc_file_1.py", "r", encoding="utf-8") as f:
                 c_enc_decrypt = f.read()
             with open(file_path_2, "w", encoding="utf-8") as f:
                 f.write(f'target_path = r"{target_path}"\n')
@@ -31,7 +31,7 @@ def c_enc_main():
             file_path = input("程序文件生成路径(文件夹路径!): ")
             file_name = input("程序文件名称(不需要后缀!): ")
             file_path_2 = f"{file_path}/{file_name}.py"
-            with open(r"c_enc_RSA\c_enc_decrypt.py", "r", encoding="utf-8") as f:
+            with open(r"plugins\c_enc_RSA\c_enc_decrypt.py", "r", encoding="utf-8") as f:
                 c_enc_decrypt = f.read()
             with open(file_path_2, "w", encoding="utf-8") as f:
                 f.write(f'target_path = r"{target_path}"\nprivate_key_path = r"{private_key_path}"\n')
@@ -41,7 +41,7 @@ def c_enc_main():
         elif choice == "4":
             break
 
-if __name__ == "__main__":
+def c_enc_main():
     print(f"{'-' * 30}")
     print("       生成加密文件工具")
     print(f"{'-' * 30}")
@@ -50,5 +50,5 @@ if __name__ == "__main__":
     print("3. 生成解密程序 - 仅解密单个文件")
     print("4. 退出")
     print(f"{'=' * 30}")
-    c_enc_main()
+    c_enc()
     print("特殊退出 Error Code[001]")
